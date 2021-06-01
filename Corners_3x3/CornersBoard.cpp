bf749be2daf91cf768b6874efb74366be668db20
yomvol <83629932+yomvol@users.noreply.github.com>
2021-06-01 13:04:53 +0300

    Pieces are set

--- Corners_3x3/CornersBoard.cpp
+++ Corners_3x3/CornersBoard.cpp
@@ -9,6 +9,12 @@ CornersBoard::CornersBoard()
 	for (unsigned int i = 0; i < boardsize; i++)
 		for (unsigned int j = 0; j < boardsize; j++)
 			tiles[i][j] = Tile_Empty;
+	for (unsigned int i = 0; i < 3; i++)
+		for (unsigned int j = 5; j < boardsize; j++)
+			tiles[j][i] = Tile_White;
+	for (unsigned int i = 5; i < boardsize; i++)
+		for (unsigned int j = 0; j < 3; j++)
+			tiles[j][i] = Tile_Black;
 }
 
 CornersBoard::~CornersBoard()
