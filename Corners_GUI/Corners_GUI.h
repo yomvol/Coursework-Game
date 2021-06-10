c474443514e6d97f04e99e8be977074ab0cbd04a
yomvol <83629932+yomvol@users.noreply.github.com>
2021-06-10 16:44:52 +0300

    Initial

---
+++ Corners_GUI/Corners_GUI.h
@@ -0,0 +1,32 @@
+﻿
+// Corners_GUI.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CCornersGUIApp:
+// See Corners_GUI.cpp for the implementation of this class
+//
+
+class CCornersGUIApp : public CWinApp
+{
+public:
+	CCornersGUIApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCornersGUIApp theApp;
---
+++ Corners_GUI/Corners_GUIDlg.h
@@ -0,0 +1,33 @@
+﻿
+// Corners_GUIDlg.h : header file
+//
+
+#pragma once
+
+
+// CCornersGUIDlg dialog
+class CCornersGUIDlg : public CDialogEx
+{
+// Construction
+public:
+	CCornersGUIDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CORNERS_GUI_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
